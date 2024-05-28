26586e42bb54ecaee9a1d6c96adfef388edeb897
Vijay John Stephen <vijay.stephen@sourcefuse.com>
2024-05-28 19:12:40 +0530

    option to add secrets/env vars

--- variables.tf
+++ variables.tf
@@ -151,6 +151,16 @@ variable "backstage_environment" {
   type        = string
   description = "Backstage environment"
 }
+variable "environment_variables" {
+  type        = list(map(string))
+  description = "(optional) List of additional environment variables"
+  default     = []
+}
+variable "secret_list" {
+  type        = list(map(string))
+  description = "(optional) List of additional Secrets"
+  default     = []
+}
 ################################################################################
 ## alb
 ################################################################################
